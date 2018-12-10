8625287af11aae7b49fd9e84eeaff6a4aff4e17f
Florian Klien <flowolf@klienux.org>
2018-12-10 12:04:26 +0100

    gitlab-ci, const docstring

--- YesssSMS/const.py
+++ YesssSMS/const.py
@@ -1,3 +1,4 @@
+"""constants for YesssSMS"""
 VERSION = "0.2.3"
 _UNSUPPORTED_CHARS_STRING = "<strong>Achtung:</strong> Ihre SMS konnte nicht \
 versendet werden, da sie folgende ungültige Zeichen enthält:"
