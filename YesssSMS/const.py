16e72243f610b13f020c86cf647a170024378679
Florian Klien <flowolf@klienux.org>
2019-09-25 14:34:05 +0200

    bump version to 0.4.1b1

--- YesssSMS/const.py
+++ YesssSMS/const.py
@@ -1,5 +1,5 @@
 """constants for YesssSMS."""
-VERSION = "0.4.1a1"
+VERSION = "0.4.1b1"
 _UNSUPPORTED_CHARS_STRING = "<strong>Achtung:</strong> Ihre SMS konnte nicht \
 versendet werden, da sie folgende ungültige Zeichen enthält:"
 _LOGIN_ERROR_STRING = "<strong>Login nicht erfolgreich"
