4e8566c154e076ae4afd15f228c47d70ff73ff82
Florian Klien <flowolf@klienux.org>
2019-10-19 18:35:12 +0200

    fix setup.py package data 🐬

--- setup.py
+++ setup.py
@@ -40,5 +40,5 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={"console_scripts": ["yessssms=YesssSMS.CLI:run"]},
-    data_files=["YesssSMS/version.json"],
+    package_data={"YesssSMS": ["version.json"]},
 )
